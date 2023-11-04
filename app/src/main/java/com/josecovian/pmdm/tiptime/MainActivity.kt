18f9f89dc7805aebc3fe6fa9baa357e4d3ab72bf
Jose Covian <jose@covian.es>
2023-11-04 16:57:51 +0100

    Fin CodeLab

--- app/src/main/java/com/josecovian/pmdm/tiptime/MainActivity.kt
+++ app/src/main/java/com/josecovian/pmdm/tiptime/MainActivity.kt
@@ -1,7 +1,11 @@
 package com.josecovian.pmdm.tiptime
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.KeyEvent
+import android.view.View
+import android.view.inputmethod.InputMethodManager
 import com.josecovian.pmdm.tiptime.databinding.ActivityMainBinding
 import java.text.NumberFormat
 
@@ -14,10 +18,11 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         binding.calculateButton.setOnClickListener{ calculateTip() }
+        binding.costOfServiceEditTest.setOnKeyListener{ view, keyCode, _ -> handleKeyEvent(view, keyCode) }
     }
 
     private fun calculateTip() {
-        val stringInTextField = binding.costOfService.text.toString()
+        val stringInTextField = binding.costOfServiceEditTest.text.toString()
         val cost = stringInTextField.toDoubleOrNull()
 
         // If the cost is null or 0, then display 0 tip and exit this function early
@@ -42,5 +47,16 @@ class MainActivity : AppCompatActivity() {
     private fun displayTip(tip: Double) {
         val formattedTip = NumberFormat.getCurrencyInstance().format(tip)
         binding.tipResult.text = getString(R.string.tip_amount, formattedTip)
+    }
+
+    private fun handleKeyEvent(view: View, keyCode: Int): Boolean {
+        if (keyCode == KeyEvent.KEYCODE_ENTER) {
+            // Hide the keyboard
+            val inputMethodManager =
+                getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
+            return true
+        }
+        return false
     }
 }
